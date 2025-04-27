831e1ed20c41ef98e478db3d7ae893e34745ff55
dalybabaygpt <dalybabaygpt@gmail.com>
2025-04-27 01:45:15 +0100

    Delete custom_addons/tashil_website_pwa directory

--- custom_addons/tashil_website_pwa/static/src/js/pwa_register.js
+++
@@ -1,6 +0,0 @@
-// Register the service worker when the page loads
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/service-worker.js');
-  });
-}
