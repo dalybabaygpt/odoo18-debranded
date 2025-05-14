84ec3fa7d48f693133f888b0ac384a6723728cfc
dalybabaygpt <dalybabaygpt@gmail.com>
2025-05-14 17:33:18 +0100

    Add files via upload

---
+++ custom_addons/app_common/app_common/hooks.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Created on 2018-10-12
+# author: 欧度智能，https://www.erp.cn
+# email: 300883@qq.com
+# resource of erp
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+# Odoo在线中文用户手册（长期更新）
+# https://www.erp.cn/documentation/user/10.0/zh_CN/index.html
+
+# Odoo10离线中文用户手册下载
+# https://www.erp.cn/erp10_user_manual_document_offline/
+# Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（erp开发必备）
+# https://www.erp.cn/erp10_developer_document_offline/
+# description:
+
+from erp import api, SUPERUSER_ID, _
+
+
+def pre_init_hook(env):
+    pass
+    # cr.execute("")
+
+def post_init_hook(env):
+    pass
+    # cr.execute("")
+
+def uninstall_hook(env):
+    pass
+    # cr.execute("")
+
