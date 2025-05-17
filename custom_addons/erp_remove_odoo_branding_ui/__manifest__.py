2107f46ce7b8a5bed94a4b609c201c334b3fe3a9
dalybabaygpt <dalybabaygpt@gmail.com>
2025-05-17 21:59:00 +0100

    POS

--- custom_addons/erp_remove_odoo_branding_ui/__manifest__.py
+++ custom_addons/erp_remove_odoo_branding_ui/__manifest__.py
@@ -1,14 +1,18 @@
 {
     "name": "ERP Remove Odoo Branding UI",
-    "version": "1.0",
-    "summary": "Removes Odoo branding from POS and replaces title/logo.",
+    "version": "1.2",
+    "summary": "Removes Odoo branding from POS by replacing title and hiding logo via CSS.",
     "author": "Custom",
-    "category": "Hidden",
+    "category": "Point of Sale",
     "depends": ["point_of_sale"],
     "data": [
-        "views/pos_cleanup.xml"
+        "views/assets.xml"
     ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "erp_remove_odoo_branding_ui/static/src/css/branding.css"
+        ]
+    },
     "installable": True,
-    "auto_install": False,
-    "application": False
+    "auto_install": False
 }
