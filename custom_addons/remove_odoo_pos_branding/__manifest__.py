752c08e3aa9fc80944ef0260f8da236f168e569c
dalybabaygpt <dalybabaygpt@gmail.com>
2025-05-17 19:38:12 +0100

    pos updated remove logo and title

--- custom_addons/remove_odoo_pos_branding/__manifest__.py
+++ custom_addons/remove_odoo_pos_branding/__manifest__.py
@@ -1,14 +1,13 @@
 {
     "name": "Remove Odoo POS Branding",
-    "version": "1.0",
-    "summary": "Removes Odoo branding from POS screen and tab title.",
+    "version": "1.1",
+    "summary": "Removes Odoo branding from POS screen and sets tab title to 'ERP POS'.",
     "author": "Custom",
     "category": "Point of Sale",
     "depends": ["point_of_sale"],
     "data": [
-        "views/assets.xml",
         "views/pos_templates.xml"
     ],
     "installable": True,
-    "application": False,
+    "application": False
 }
