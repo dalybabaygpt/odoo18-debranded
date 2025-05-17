bbcf31b2ba276b4d14ffeedf9ac3ce16a22648bc
dalybabaygpt <dalybabaygpt@gmail.com>
2025-05-17 19:23:11 +0100

    Added Addons
    Backend theme Clarity
    POS remove logo and title change from odoo pos to erp pos

---
+++ custom_addons/remove_odoo_pos_branding/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Remove Odoo POS Branding",
+    "version": "1.0",
+    "summary": "Removes Odoo branding from POS screen and tab title.",
+    "author": "Custom",
+    "category": "Point of Sale",
+    "depends": ["point_of_sale"],
+    "data": [
+        "views/assets.xml",
+        "views/pos_templates.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
