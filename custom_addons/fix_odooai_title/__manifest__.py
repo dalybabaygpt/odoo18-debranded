53683596a329ee43112fe664ab9e72eee5eb91a4
dalybabaygpt <dalybabaygpt@gmail.com>
2025-05-26 00:32:00 +0100

    rr

---
+++ custom_addons/fix_odooai_title/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Fix OdooAI Title",
+    "version": "1.0",
+    "summary": "Removes flashing 'Odoo' or 'odooai' from browser title during load",
+    "author": "ChatGPT Custom",
+    "category": "Web",
+    "depends": ["web"],
+    "data": [
+        "views/webclient_title_fix.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3"
+}
