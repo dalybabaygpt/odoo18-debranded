31648a87d1d525a0b5e7f70b0b6df15bf5c7c4d9
dalybabaygpt <dalybabaygpt@gmail.com>
2025-05-26 00:00:39 +0100

    rename ERP

---
+++ custom_addons/rename_to_erp/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Rename to Erp",
+    "version": "1.0",
+    "summary": "Replace Odoo branding with Erp or Erpai",
+    "category": "Customization",
+    "author": "ChatGPT",
+    "website": "https://erp.local",
+    "depends": ["base"],
+    "data": ["views/res_config_settings_view.xml"],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
