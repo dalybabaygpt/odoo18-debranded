4c1dc19fbe6d446eb0617baaa1f7f6ff64f396f8
dalybabaygpt <dalybabaygpt@gmail.com>
2025-05-26 00:20:16 +0100

    ren

---
+++ custom_addons/rename_to_erp_fullbranding/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Rename to Erp Full Branding",
+    "version": "18.0.1.0.0",
+    "category": "Customization",
+    "summary": "Replaces Odoo and OdooAI branding with Erp and Erpai",
+    "author": "Daly Custom",
+    "depends": ["base", "web"],
+    "data": [
+        "views/webclient_templates.xml",
+        "data/system_parameters.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3"
+}
