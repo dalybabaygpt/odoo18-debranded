a53cbd434f3264ed823b791e0d1312f5a962a531
dalybabaygpt <dalybabaygpt@gmail.com>
2025-05-26 00:35:55 +0100

    rr

---
+++ custom_addons/fix_odoo_title_corrected/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Fix Odoo Title - Corrected",
+    "version": "1.0",
+    "summary": "Fixes browser tab title to prevent flashing odooai title",
+    "category": "Web",
+    "author": "Custom GPT",
+    "depends": ["web"],
+    "data": [
+        "views/webclient_title_patch.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3"
+}
