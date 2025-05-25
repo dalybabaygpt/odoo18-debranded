3cf8f6b76d0d3949db21bd3e8a36f2143d265c27
dalybabaygpt <dalybabaygpt@gmail.com>
2025-05-25 17:59:57 +0100

    pos splah

---
+++ custom_addons/pos_idle_screen_clean/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "POS Idle Screen Clean",
+    "version": "1.0",
+    "summary": "Replace POS idle screen with custom background and message",
+    "author": "Custom",
+    "category": "Point of Sale",
+    "depends": ["point_of_sale"],
+    "assets": {
+        "point_of_sale.assets": [
+            "pos_idle_screen_clean/static/src/xml/pos_idle_screen.xml"
+        ]
+    },
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3"
+}
